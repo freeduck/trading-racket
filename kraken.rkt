07dd6a312f531835383f678f871f935b48bed293
Kristian Nygaard Jensen <kristian@freeduck.dk>
2019-01-04 23:20:10 +0100

    Starting to build POST request

--- kraken.rkt
+++ kraken.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require http/request)
 (require net/http-client json)
+(require net/uri-codec)
 (define (list-assets)
   (define-values (in out) (connect "http" "www.google.com" 80))
   (println in)
@@ -10,5 +11,18 @@
 (define (asset-info)
   (define-values (status header response)
     (http-sendrecv "api.kraken.com" "/0/public/Assets" #:ssl? 'tls))
+  (define data (read-json response))
+  (println data))
+
+(define (balance)
+  (define post-data (list
+                     (cons 'nonse (number->string
+                                   (current-milliseconds)))))
+  (define-values (status header response)
+    (http-sendrecv "api.kraken.com"
+                   "/0/private/Balance"
+                   #:ssl? 'tls
+                   #:method "POST"
+                   #:data (alist->form-urlencoded post-data)))
   (define data (read-json response))
   (println data))
