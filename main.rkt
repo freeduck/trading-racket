29e7ad690128078576deb926a0d5534ad366c632
Kristian Nygaard Jensen <kristian@freeduck.dk>
2019-01-15 15:37:18 +0100

    Test signature

--- main.rkt
+++ main.rkt
@@ -32,12 +32,35 @@
   ;; Tests to be run with raco test
   )
 
+(define (create-config yaml-config-path)
+  (define config-yaml (file->yaml (path->string (expand-user-path yaml-config-path))))
+  (lambda (key)
+    (hash-ref config-yaml key)))
+
 (module+ main
-  (define (create-config yaml-config-path)
-    (define config-yaml (file->yaml (path->string (expand-user-path yaml-config-path))))
-    (lambda (key)
-      (hash-ref config-yaml key)))
   (define get-config (create-config "~/kraken.yaml"))
   ;; Main entry point, executed when run with the `racket` executable or DrRacket.
   (bytes->string/latin-1 (base64-decode (string->bytes/latin-1 (get-config "secret"))))
   (println "Hello, World!"))
+
+(module+ test
+  ;; Signature data
+  ;; Postdata
+  ;; hest=hjort&nonce=1547494295437
+  ;; Encoded
+  ;; b'1547494295437hest=hjort&nonce=1547494295437'
+  ;; Message
+  ;; b'/0/private/Balance,5\x8b\xfb\xab\xd5\x81L\xfc~P>\x19A\x81\xe3s\xc6\x87\x98\t\xe0\xac\xcf\x0e\x98\x7fK\xc5z\x0c\xb4'
+  ;; Signature
+  ;; <hmac.HMAC object at 0x7fda917935c0>
+  ;; Sigdigest
+  ;; b'U1ELyaQAlkTY8ABOQ1YZ6TbiWf7oHYNMCL9Z+fQEV/Zb0MR7xAUnwKkczuh8fewShhWV94eWLpQENZZ8qUeCeA=='
+  ;; Sigdigest decoded
+  ;; <built-in method decode of bytes object at 0x7fda932488b0>
+  ;; U1ELyaQAlkTY8ABOQ1YZ6TbiWf7oHYNMCL9Z+fQEV/Zb0MR7xAUnwKkczuh8fewShhWV94eWLpQENZZ8qUeCeA==
+  (require rackunit)
+  (define get-config (create-config "~/kraken.yaml"))
+  (define postdata (list '["nonce" . "1547494295437"]
+                         '["hest" . "hjort"]))
+  (check-equal? "Py9hOynphm+Z2504vLZGd9gEwtjdFTHztONLN83Wv30gQzmjIILO6b+rfFmFxB1XspYD2jTBCv4K4eSoGepfcQ=="
+                (sign postdata '/0/private/Balance' (get-config "secret"))))
