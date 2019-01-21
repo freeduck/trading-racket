64488177af953669bc53de6eda5a96415e296cb5
Kristian Nygaard Jensen <kristian@freeduck.dk>
2019-01-21 09:13:52 +0100

    Test config function

---
+++ config.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(require yaml)
+(define (create-config yaml-config-path)
+  (define config-yaml (file->yaml (path->string (expand-user-path yaml-config-path))))
+  (lambda (key)
+    (hash-ref config-yaml key)))
+
+(module+ test
+  (require rackunit)
+  (define get-config (create-config "credentials_yaml_fixture.yaml"))
+  (check-equal? (get-config "user")
+                1)
+  (check-equal? (get-config "pass")
+                2))
