309322e8949f7b089ac67a732bf99de3e7bd7940
Kristian Nygaard Jensen <kristian@freeduck.dk>
2019-01-28 21:39:05 +0100

    Cleaning up

--- crypto.rkt
+++ crypto.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(require net/uri-codec net/base64 sha)
+(require  sha)
 (provide sha-256)
 
 (define (sha-256 str)
@@ -7,6 +7,6 @@
    (string->bytes/utf-8 str)))
 
 (module+ test
-  (require rackunit)
+  (require rackunit net/uri-codec net/base64)
   (check-equal? (bytes->string/utf-8 (base64-encode (sha-256 "1547494295437hest=hjort&nonce=1547494295437")))
                 "LDWL+6vVgUz8flA+GUGB43PGh5gJ4KzPDph/S8V6DLQ=\r\n"))
