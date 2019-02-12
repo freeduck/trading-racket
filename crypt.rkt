d74409679e5c2c744dd9601d45fe313447bae5ec
Kristian Nygaard Jensen <kristian@devuan-x200-knj.kringer.dk>
2019-02-12 22:45:15 +0100

    Message signing complete

--- crypt.rkt
+++ crypt.rkt
@@ -11,24 +11,25 @@
 
 (define (sign data path secret)
   (define postdata (alist->form-urlencoded data))
-  (define bpath (string->bytes/utf-8 path))
-  (define strdata (string-append (cdr (assoc 'nonce
-                                             data))
+  (define payload (string-append (cdr (assoc 'nonce data))
                                  postdata))
-  (define bdata (sha-256 strdata))
-  (define prefixed (bytes-append bpath
-                                 bdata))
-  (define signature (hmac 'sha256 (base64-decode (string->bytes/utf-8 secret))
+  (println (string-append "Payload: " payload))
+  (define hashed (sha-256 payload))
+  (println (string-append "Hashed 64: " (string-trim (bytes->string/utf-8 (base64-encode hashed)))))
+  (define prefixed (bytes-append (string->bytes/utf-8 path) hashed))
+  (println (string-append "Prefix 64: " (bytes->string/utf-8 (base64-encode prefixed))))
+
+  (define signature (hmac 'sha512 (base64-decode (string->bytes/utf-8 secret))
                                  prefixed))
-  signature)
+  (bytes->string/utf-8 (base64-encode signature "")))
 
 (module+ test
   (require rackunit net/uri-codec net/base64)
   (check-equal? (bytes->string/utf-8 (base64-encode (sha-256 "1547494295437hest=hjort&nonce=1547494295437")))
                 "LDWL+6vVgUz8flA+GUGB43PGh5gJ4KzPDph/S8V6DLQ=\r\n")
-  (define postdata '([hest . "hjort"]
-                     [nonce . "1547494295437"]))
+  (define postdata '([nonce . "1547494295437"]
+                     [hest . "hjort"]))
   (define path "/0/private/Balance")
   (define secret "2hC9xDAdhO287Y9DN4VSlfk3KwDKU0HKjSZbgteNZPA=")
-  (check-equal? (bytes->string/utf-8 (base64-encode (sign postdata path secret)))
-                "LDWL+6vVgUz8flA+GUGB43PGh5gJ4KzPDph/S8V6DLQ=\r\n"))
+  (check-equal? (sign postdata path secret)
+                "78n0913GO6B7UFaSAQhVw8/xqTI1KV8J1S9l7Y6SYPCbhSl/AFl3zgng7amMbm464ydSmCi38cuO3gTRPOZrRg=="))
