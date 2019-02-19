2d3fde8d5ab7e3ddcdd3ef08767066038a8816a5
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-02-19 17:48:08 +0100

    Plot all data between 2018-11-18-22:21:00 and 2019-02-18-22:21:00

---
+++ plot-ohlc.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(require plot)
+;; (plot-new-window? #t)
+;; (require plot/snip)
+
+(provide plot-with-x-as-time (all-from-out plot))
+
+(define (plot-with-x-as-time xy)
+  (parameterize ([plot-x-ticks (time-ticks)]
+                 [plot-new-window? #t])
+    (plot xy)))
---
+++ plot.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(require db
+         "plot-ohlc.rkt")
+(define *db*
+  (sqlite3-connect #:database
+                   "2018-11-18-22:21:00-2019-02-18-22:21:00.db"))
+
+(define rows (query-rows *db*
+                         "select start,open from candles_EUR_XMR"))
+(plot-with-x-as-time (list (points rows)))
