c4eccf25218d061cde24de6293b54a58595c704c
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-02-21 23:42:59 +0100

    Dimensions

--- plot.rkt
+++ plot.rkt
@@ -6,11 +6,12 @@
 (define *db*
   (sqlite3-connect #:database
                    "2018-11-18-22:21:00-2019-02-18-22:21:00.db"))
-
+(define x-min 1542579840)
+(define x-max (+ 1542579840 (* 3600 40)))
 (define rows (query-rows *db*
                          "select start,open from candles_EUR_XMR where start >= $1 and start < $2"
-                         1542579840
-                         (+ 1542579840 (* 3600 4))))
+                         x-min
+                         x-max))
 
 (define (mouse-callback snip event x y)
   (if (and x y)
@@ -21,11 +22,13 @@
           (println  (round x))))
 
       (send snip set-overlay-renderers #f)))
-
+(define y-min (vector-ref (argmin (lambda (v)(vector-ref v 1)) rows) 1))
+(define y-max (vector-ref (argmax (lambda (v)(vector-ref v 1)) rows) 1))
 
 (define  (make-2d-plot-snip data width height)
   (define snip (plot-snip (list (points rows))
-                          #:x-min 1542579840 #:x-max (+ 1542579840 (* 3600 4))))
+                          #:x-min x-min #:x-max x-max
+                          #:y-min y-min #:y-max y-max))
   (send snip set-mouse-event-callback mouse-callback)
   snip)
 
