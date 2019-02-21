c1e809a3754bcfbdd75c0e026711be9e0642fd23
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-02-21 22:22:08 +0100

    Return plot snip

--- plot-ohlc.rkt
+++ plot-ohlc.rkt
@@ -6,5 +6,5 @@
 (provide plot-with-x-as-time (all-from-out plot))
 
 (define (plot-with-x-as-time xy)
-  (parameterize ([plot-x-ticks (time-ticks)])
-    (plot xy)))
+  (parameterize ([plot-x-ticks (date-ticks)])
+    (plot-snip xy)))
