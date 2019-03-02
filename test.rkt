d8deb986d6de836052ec61c1325911a76a43c7c7
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-03-02 13:52:44 +0100

    Cleaning up scan window

--- test.rkt
+++ test.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require db
-         "fit.rkt")
+         "fit.rkt"
+         "data.rkt")
 (define *db*
   (sqlite3-connect #:database
                    "2018-11-18-22:21:00-2019-02-18-22:21:00.db"))
@@ -9,18 +10,15 @@
 
 (define hour-past-first-fit 1542734640)
 
-(define (scan-window start end)
+(define (scan-window start end row-db)
   (for/fold ([peak #f])
             ([current (in-range (+ start 600) end 600)]
              #:break peak)
-    (let* ([rows (query-rows *db*
-                             "select start,open from candles_EUR_XMR where start >= $1 and start < $2"
-                             start
-                             current)]
+    (let* ([rows (row-db start current)]
            [peak (peak-at rows)])
       (when peak
         peak))))
 
 
 (define (first-peak)
-  (scan-window latest-trade hour-past-first-fit))
+  (scan-window latest-trade hour-past-first-fit (select-window *db*)))
