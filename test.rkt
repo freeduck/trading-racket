f5b4dfa89044adb4fe0c74265609d3f6121f40ce
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-03-02 13:30:53 +0100

    Move loop into scann function

--- test.rkt
+++ test.rkt
@@ -4,15 +4,23 @@
 (define *db*
   (sqlite3-connect #:database
                    "2018-11-18-22:21:00-2019-02-18-22:21:00.db"))
+
 (define latest-trade 1542579840)
+
 (define hour-past-first-fit 1542734640)
-(for/fold ([peak #f])
-          ([end (in-range (+ latest-trade 600) hour-past-first-fit 600)]
-           #:break peak)
-  (let* ([rows (query-rows *db*
-                          "select start,open from candles_EUR_XMR where start >= $1 and start < $2"
-                          latest-trade
-                          end)]
-         [peak (peak-at rows)])
-    (when peak
-        (println peak))))
+
+(define (scan-window start end)
+  (for/fold ([peak #f])
+            ([current (in-range (+ start 600) end 600)]
+             #:break peak)
+    (let* ([rows (query-rows *db*
+                             "select start,open from candles_EUR_XMR where start >= $1 and start < $2"
+                             start
+                             current)]
+           [peak (peak-at rows)])
+      (when peak
+        peak))))
+
+
+(define (first-peak)
+  (scan-window latest-trade hour-past-first-fit))
