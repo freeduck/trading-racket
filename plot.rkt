4cdeae66a67316b20d0b9bf585202c2a83fc1e94
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-03-03 14:26:46 +0100

    Cleaning up plot

--- plot.rkt
+++ plot.rkt
@@ -32,15 +32,14 @@
   (send toplevel show #t))
 
 (module+ test
+  (require "data.rkt")
   (define *db*
     (sqlite3-connect #:database
                      "2018-11-18-22:21:00-2019-02-18-22:21:00.db"))
+  (define data-source (select-window *db*))
   (define x-min 1542579840)
   (define x-max (+ 1542579840 (* 3600 42)))
-  (define rows (query-rows *db*
-                           "select start,open from candles_EUR_XMR where start >= $1 and start < $2"
-                           x-min
-                           x-max))
+  (define rows (data-source x-min x-max))
   (define y-min (vector-ref (argmin (lambda (v)(vector-ref v 1)) rows) 1))
   (define y-max (vector-ref (argmax (lambda (v)(vector-ref v 1)) rows) 1))
   (println "Peak at")
