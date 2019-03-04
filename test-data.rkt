29e95c62c3767c9f552cb5b0212232940c3f47b6
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-03-04 15:11:34 +0100

    Use test data in notes

--- test-data.rkt
+++ test-data.rkt
@@ -1,8 +1,13 @@
+#lang racket
+(require db
+         crypto-trading/data)
+(provide data-source first-trade second-trade-target)
 (define *db*
   (sqlite3-connect #:database
                    "2018-11-18-22:21:00-2019-02-18-22:21:00.db"))
 
-(require crypto-trading/data)
 (define data-source (select-window *db*))
+
 (define first-trade 1542579840) ; found by hand
-(define x-max (+ 1542579840 (* 3600 42)))
+;; The second trade should be around here
+(define second-trade-target (+ 1542579840 (* 3600 42)))
