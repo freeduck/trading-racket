4c2392c673a9761be23489a9021d39933cb9ccf9
Kristian Nygaard jensen <freeduck@member.fsf.org>
2019-03-05 15:40:51 +0100

    Document fit function

--- fit.rkt
+++ fit.rkt
@@ -5,7 +5,7 @@
 
 (define xs '(0 1  2  3  4  5   6   7   8   9  10))
 (define ys '(1 6 17 34 57 86 121 162 209 262 321))
-
+;; Polynomial/Multiple regression
 (define (fit x y n)
   (define Y (->col-matrix y))
   (define V (vandermonde-matrix x (+ n 1)))
