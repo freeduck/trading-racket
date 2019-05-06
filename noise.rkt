ac7afd2cd3bd0cd67cefad68c39617784fdf7711
Martin Nygaard Jensen <martin@debian-t400>
2019-05-07 00:19:37 +0200

    Remove noise

--- noise.rkt
+++ noise.rkt
@@ -4,16 +4,25 @@
          crypto-trading/plot
          crypto-trading/math)
 
-(define (find-advice  time-series #:step [step 600] [window-size step] #:scope [scope take] [advice #f])
+(define (find-advice  time-series #:step [step 600] [window-size step] #:scope [scope take] #:advice [advice #f])
   (with-handlers ([exn:fail:contract? (lambda (exn)
                                         #f)])
-    (if advice
+    (begin
+      (displayln advice)
+      (displayln (get-advice time-series))
+      (if (get-advice time-series)
+          (displayln "ad")
+          (displayln "no"))
+      (if advice
         advice
         (find-advice time-series
                      #:step step
                      (+ step window-size)
-                     (get-advice (scope time-series window-size))
-                     #:scope scope))))
+                     #:advice (get-advice (scope time-series window-size))
+                     #:scope scope)))))
 
 (define (remove-noise)
-  (find-advice #:scope drop (test-data-source noise-start aprox-peak-after-noise)))
+  (find-advice #:scope drop #:step 150 (test-data-source noise-start aprox-peak-after-noise))) ;
+
+(define (plot-noise)
+  (plot-on-frame (lines (test-data-source noise-start aprox-peak-after-noise))))
