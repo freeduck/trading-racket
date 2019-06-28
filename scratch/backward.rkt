00f03fd2c988614c77522438c2f053661169e698
Kristian Nygaard Jensen <kristian@devuan-x200.kringer.dk>
2019-06-28 08:30:33 +0200

    Right to left

--- scratch/backward.rkt
+++ scratch/backward.rkt
@@ -1,6 +1,13 @@
 #lang racket
-(require "../data-mangling.rkt"
-         "../test.rkt")
+(require "../fit.rkt"
+         "../data-mangling.rkt"
+         "../test.rkt"
+         plot)
 (parameterize ([data-path ".."])
-  (define rev-sliced-data (reverse (slice-data (test-data-source noise-start aprox-noise-end))))
-  (displayln rev-sliced-data))
+  (define rev-sliced-data (reverse (slice-data (test-data-source noise-start aprox-peak-after-noise))))
+  (let-values ([(peak data-series) (for/fold ([peak #f] [old-data '()])
+                                             ([bin (in-list rev-sliced-data)]
+                                              #:break peak)
+                                     (let ([data (append bin old-data)])
+                                       (values (find-peak data) data)))])
+    (plot (lines data-series))))
