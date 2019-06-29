40fa2bc18bf6a8d2d36d50a6ea358f3ac4d26983
Kristian Nygaard Jensen <kristian@devuan-x200.kringer.dk>
2019-06-30 00:06:52 +0200

    Extract find-peak-in-list

--- scratch/backward.rkt
+++ scratch/backward.rkt
@@ -3,11 +3,15 @@
          "../data-mangling.rkt"
          "../test.rkt"
          plot)
+
+(define (find-peak-in-list data-source)
+  (for/fold ([peak #f] [old-data '()])
+            ([bin (in-list data-source)]
+             #:break peak)
+    (let ([data (append bin old-data)])
+      (values (find-peak data) data))))
+
 (parameterize ([data-path ".."])
   (define rev-sliced-data (reverse (slice-data (test-data-source noise-start aprox-peak-after-noise))))
-  (let-values ([(peak data-series) (for/fold ([peak #f] [old-data '()])
-                                             ([bin (in-list rev-sliced-data)]
-                                              #:break peak)
-                                     (let ([data (append bin old-data)])
-                                       (values (find-peak data) data)))])
+  (let-values ([(peak data-series) (find-peak-in-list rev-sliced-data)])
     (plot (lines data-series))))
