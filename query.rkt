ecf8bb1e7127a3f1962a56a50b7b24a44c3fc87a
Kristian Nygaard Jensen <kristian@devuan-x200.kringer.dk>
2019-07-10 21:50:45 +0200

    Cleaning up prize within threshold

--- query.rkt
+++ query.rkt
@@ -1,18 +1,26 @@
 #lang racket
 (require threading)
-(provide first-prize-in-series last-prize-in-series)
+(provide prize-delta
+         first-prize-in-series
+         last-prize-in-series)
+
+(define (prize-delta time-series)
+  (abs (- (last-prize-in-series time-series) (first-prize-in-series time-series))))
 
 (define (first-prize-in-series time-series)
   (~> time-series
+      sequence->list
       first
       get-prize))
 
 (define (last-prize-in-series time-series)
   (~> time-series
+      sequence->list
       last
       get-prize))
 
 (define (get-prize vec)
   (vector-ref vec 1))
+
 
 
--- scratch/backward.rkt
+++ scratch/backward.rkt
@@ -14,18 +14,12 @@
                                        (values (find-peak data #:validate-fn validate-peak-fn) data)))])
     peak))
 
-(define (within-prize-threshold? a-time-series (threshold 0.02))
-  (let* ([time-series (if (list? a-time-series)
-                          a-time-series
-                          (sequence->list a-time-series))]
-         [first-prize (first-prize-in-series time-series)]
-         [last-prize (last-prize-in-series time-series)]
-         [prize-delta (abs (- last-prize first-prize))])
-    (if (> (* threshold
-              first-prize)
-           prize-delta)
-        #f
-        a-time-series)))
+(define (within-prize-threshold? time-series (threshold 0.02))
+  (let ([numeric-threshold (* threshold (first-prize-in-series time-series))])
+    (if (< (prize-delta time-series)
+           numeric-threshold)
+        time-series
+        #f)))
 
 (define (validate-peak peak-analysis)
   (and~> peak-analysis
