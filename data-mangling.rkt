0e7aba634a1efe7b1a0f1430607d89df381542d5
Kristian Nygaard Jensen <kristian@devuan-x200.kringer.dk>
2019-07-15 23:54:33 +0200

    Append slices

--- data-mangling.rkt
+++ data-mangling.rkt
@@ -4,6 +4,7 @@
 
 (provide
  slice-data
+ append-slices
  prepend-slices
  transpose)
 
@@ -15,6 +16,14 @@
              (for/fold ([data-accum '()])
                        ([slice (in-list (reverse slices))])
                (let ([cur-data (append slice data-accum)])
+                 (yield cur-data)
+                 cur-data))))
+
+(define (append-slices slices)
+  (in-generator
+             (for/fold ([data-accum '()])
+                       ([slice (in-list slices)])
+               (let ([cur-data (append data-accum slice)])
                  (yield cur-data)
                  cur-data))))
 
