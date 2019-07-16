4d78c030adb3df5b74b98d70a7f28479f6557b07
Kristian Nygaard Jensen <kristian@devuan-x200.kringer.dk>
2019-07-16 22:08:14 +0200

    Move plot out of loop and experiment with modules

--- scratch/forward.rkt
+++ scratch/forward.rkt
@@ -6,11 +6,15 @@
          "../plot.rkt"
          threading)
 (module+ test
-  (parameterize ([data-path ".."])
-    (for/first ([part (~> (test-data-source first-trade second-trade-target)
-                          slice-data
-                          append-slices)]
-                #:when (and~> part
-                              data-set->parabola
-                              validate-peak))
-      (plot (lines part)))))
+  (require rackunit)
+  (module+ main
+    (displayln "Hest"))
+  (module+ scratch
+    (parameterize ([data-path ".."])
+      (plot (lines (for/first ([part (~> (test-data-source first-trade second-trade-target)
+                                         slice-data
+                                         append-slices)]
+                               #:when (and~> part
+                                             data-set->parabola
+                                             validate-peak))
+                     part))))))
