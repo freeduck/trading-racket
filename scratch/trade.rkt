3953a4548e424da033a23b1b24ee194f0ce8252d
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-09-04 23:16:01 +0200

    Sum is working

--- scratch/trade.rkt
+++ scratch/trade.rkt
@@ -13,40 +13,48 @@
                        (peaks)))
 
   (module+ trade
-    (define (trade amount prize account)
-      (displayln "trade")
-      (displayln amount)
-      (displayln (xmr? amount))
-      (displayln (eur? amount))
-      (displayln (xmr? prize))
-      (displayln prize)
-      (let ([total (* (amount) (prize))])
-        (if (and (xmr? amount)
-                 (eur? prize))
-            (hash-set* account
-                                         xmr (- (hash-ref account xmr)
-                                                (amount))
-                                         eur (+ (hash-ref account eur)
-                                                total))
-            (hash-set* account
-                                         xmr (+ (hash-ref account xmr)
-                                                (amount))
-                                         eur (- (hash-ref account eur)
-                                                total)))))
+    ;; (define (trade amount prize account)
+    ;;   (let ([total (* (amount) (prize))])
+    ;;     (if (and (xmr? amount)
+    ;;              (eur? prize))
+    ;;         (hash-set* account
+    ;;                                      xmr (- (hash-ref account xmr)
+    ;;                                             (amount))
+    ;;                                      eur (+ (hash-ref account eur)
+    ;;                                             total))
+    ;;         (hash-set* account
+    ;;                                      xmr (+ (hash-ref account xmr)
+    ;;                                             (amount))
+    ;;                                      eur (- (hash-ref account eur)
+    ;;                                             total)))))
 
-    (define (sell amount prize account)
-      (trade amount prize account))
-    (define (buy amount prize account)
-      (trade prize amount account))
-    (for/fold ([acc (hash eur 100
-                          xmr 20)])
-              ([peak peak-seq])
-      (let* ([peak-data (sequence->list peak)]
-             [opening-prize (vector-ref (first peak-data) 1)]
-             [closing-prize (vector-ref (last peak-data) 1)]
-             [diff (- closing-prize opening-prize)]
-             [amount (xmr 5)]
-             [prize (eur closing-prize)])
-        (if (> diff 0)
-            (sell amount prize acc)
-            (buy amount prize acc))))))
+    ;; (define (sell amount prize account)
+    ;;   (trade amount prize account))
+    ;; (define (buy amount prize account)
+    ;;   (trade prize amount account))
+    (define (trade type amount prize account)
+      (let ([total (* amount prize)])
+        (if (equal? 'sell type)
+            (hash-set* account
+                       eur (+ (hash-ref account eur)
+                              total)
+                       xmr (- (hash-ref account xmr)
+                              amount))
+            (hash-set* account
+                       eur (- (hash-ref account eur)
+                              total)
+                       xmr (+ (hash-ref account xmr)
+                              amount)))))
+    (define (sum)
+      (for/fold ([acc (hash eur 100
+                            xmr 20)])
+                ([peak peak-seq])
+        (let* ([peak-data (sequence->list peak)]
+               [opening-prize (vector-ref (first peak-data) 1)]
+               [closing-prize (vector-ref (last peak-data) 1)]
+               [diff (- closing-prize opening-prize)]
+               [amount 5]
+               [prize closing-prize])
+          (if (> diff 0)
+              (trade 'sell amount prize acc)
+              (trade 'buy amount prize acc)))))))
