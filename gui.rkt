0d7a49c803f58563a536f1b2ed1602052b766f93
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-11-18 20:59:38 +0100

    Starting to work on plotting gui

--- gui.rkt
+++ gui.rkt
@@ -3,6 +3,49 @@
 
 ; Make a frame by instantiating the frame% class
 (define frame (new frame% [label "Example"]))
- 
-; Show the frame by calling its show method
-(send frame show #t)
+(define panel (new horizontal-panel% [parent frame]))
+(module+ play
+  ; Make a static text message in the frame
+  (define msg (new message% [parent frame]
+                   [label "No events so far..."]))
+  
+  ; Make a button in the frame
+  (new button% [parent frame]
+       [label "Click Me"]
+       ; Callback procedure for a button click:
+       [callback (lambda (button event)
+                   (send msg set-label "Button click"))])
+
+  (new button% [parent panel]
+       [label "Left"]
+       [callback (lambda (button event)
+                   (send msg set-label "Left click"))])
+  (new button% [parent panel]
+       [label "Right"]
+       [callback (lambda (button event)
+                   (send msg set-label "Right click"))])
+
+  (new button% [parent frame]
+       [label "Pause"]
+       [callback (lambda (button event) (sleep 5))])
+
+  ;; (new canvas% [parent fram]
+  ;;      [paint-callback (lambda (dc)
+  ;;                        (let ([data '()])))])
+
+  ; Show the frame by calling its show method
+  (send frame show #t))
+
+(module+ plot
+  (require plot
+           mrlib/snip-canvas)
+  (new button% [parent panel]
+       [label "Right"]
+       [callback (lambda (button event)
+                   (send button set-label "Right click"))])
+  (new )
+  (new snip-canvas%
+                      [parent panel]
+                      [make-snip (lambda (width height)
+                                   (make-2d-plot-snip width height plotables))])
+  (send frame show #t))
