7e7d383dc04e87a359667101f47b2c10fd3a1f84
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-11-19 21:58:57 +0100

    plot 2018

--- gui.rkt
+++ gui.rkt
@@ -34,11 +34,17 @@
 
 (module+ plot
   (require plot
-           mrlib/snip-canvas)
+           mrlib/snip-canvas
+           db
+           "data.rkt")
+  (define database "/home/kristian/projects/crypto-trading/2018-11-18-22:21:00-2019-02-18-22:21:00.db")
+  (define con (sqlite3-connect #:database database))
+  (define data-source (select-window con))
   (let* ([x (build-list 10 values)]
          [y (list 2.7 2.8 31.4 38.1 58.0 76.2 100.5 130.0 149.3 180.0)]
          [y2 (list 2.7 2.8 131.4 38.1 158.0 76.2 100.5 130.0 149.3 180.0)]
-         [plotables (box (points (map vector x y)))]
+         ;; [plotables (box (points (map vector x y)))]
+         [plotables (box (points (data-source)))]
          [plt (new snip-canvas% [parent panel]
                    [make-snip (lambda (width height)
                                 (plot-snip (unbox plotables)))])]
