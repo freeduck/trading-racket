3eae879f471e495bfbf01ea974c9e76f0555f689
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-11-30 13:40:15 +0100

    Extract offset

--- peak.rkt
+++ peak.rkt
@@ -17,8 +17,7 @@
     part))
 (define (validate-peak para)
   (let*-values (([first-x first-y last-x last-y] (dimensions para))
-                ([offset] (+ (* 0.2 (- last-x first-x))
-                            first-x))
+                ([offset] (offset first-x last-x))
                 ([focus-x] (parabola-focus-x para)))
     (if (and (<= offset focus-x last-x)
              (< 1.5 (abs (- last-y first-y))))
@@ -32,6 +31,10 @@
         [last-coord (sequence-ref window (- (sequence-length window) 1))])
     (values (vector-ref first-coord 0) (vector*-ref first-coord 1)
             (vector-ref last-coord 0) (vector*-ref last-coord 1))))
+
+(define (offset first-x last-x)
+  (+ (* 0.5 (- last-x first-x))
+                            first-x))
 (module+ test
   (require "test.rkt"
            rackunit
@@ -40,6 +43,6 @@
                        (peaks)))
   (define first-peak (sequence-ref peak-seq 0))
   (let-values (([first-x first-y last-x last-y] (dimensions first-peak)))
-    (display-lines (list first-x first-y last-x last-y)))
-  ;; (plot (lines (sequence-ref peak-seq 0)))
-  )
+    (display-lines (list first-x first-y last-x last-y))
+    (displayln (offset first-x last-x))
+    (plot (lines first-peak))))
