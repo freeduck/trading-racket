349688194909d88a494c3990cb729ce12f9e8959
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-12-03 15:34:24 +0100

    Starting to build analysis

---
+++ analysis.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(require sql)
+(define  trade-analysis-table "trade_analysis")
+(define ensure-trade-analysis-table (create-table #:if-not-exists (Ident:AST ,(make-ident-ast trade-analysis-table))
+                                                  #:columns
+                                                  [id integer #:not-null]
+                                                  [x integer #:not-null]
+                                                  [y integer #:not-null]
+                                                  #:constraints (primary-key id)))
+(module+ test
+  (require rackunit
+           db
+           sql
+           "test-data.rkt"
+           "peak.rkt"
+           "data.rkt")
+  (define peak-seq (peaks (select-single-ohlc-field)))
+  (define con (make-temp-con))
+  (query-exec con ensure-trade-analysis-table))
