71aa2adb932d1aa4050269d91216ffd68bf0c432
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2019-12-20 15:19:59 +0100

    debugging

--- analysis.rkt
+++ analysis.rkt
@@ -18,20 +18,32 @@
   (insert #:into (Ident:AST ,(make-ident-ast (trade-analysis-table)))
           #:set [epoc ,epoc]))
 
-(define (trade-epoc window)
-  (~> window
-      (sequence-ref 0)
-      (vector-ref 0)))
+(define trade-epoc
+  (lambda~> sequence->list
+            last
+            (vector-ref 0)))
 (module+ test
   (module+ to-json
     (define (first-ten)
       (define peak-seq (peaks (select-single-ohlc-field)))
-      (define the-ten (stream->list (stream-take (for/stream ([p (sequence->stream peak-seq)])(trade-epoc p)) 10)))
+      (define the-ten (for/list ([p peak-seq])(trade-epoc p)))
       (with-output-to-file "trade.json"
         (λ () (printf (jsexpr->string the-ten))))
       ;; (with-output-to-file some-file
     ;; (lambda () (printf "hello world")))
       ))
+  (module+ iterate
+    (define raw-data (parameterize ([data-path "/home/kristian/projects/gekko/history"]
+                                    [*db* "kraken_0.1.db"])
+                       (select-single-ohlc-field)))
+    (define data-seq (in-slice 600 (in-list raw-data)))
+    (define (print)
+      (for/list ([s (in-sequences data-seq)])s)))
+  (module+ speed
+    (require db)
+    (define kraken-db (sqlite3-connect #:database
+                                       "/home/kristian/projects/gekko/history/kraken_0.1.db"))
+    (for/fold ([cnt 0])([(point-in-time prize) (in-query kraken-db "select start,open from candles_EUR_XMR")])(+ cnt prize)))
   (require rackunit
            db
            sql
